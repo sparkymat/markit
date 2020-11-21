60de2461fdf722957f98373d7fb1ce1b607884c3
Ajith Hussain <ajith.hussain@fastmail.com>
2020-11-21 15:53:23 +1100

    Bookmarks page

--- view/page/bookmark_home.qtpl.go
+++ view/page/bookmark_home.qtpl.go
@@ -5,49 +5,56 @@
 package page
 
 //line view/page/bookmark_home.qtpl:1
+import "github.com/sparkymat/markit/view/partial"
+
+//line view/page/bookmark_home.qtpl:3
 import (
 	qtio422016 "io"
 
 	qt422016 "github.com/valyala/quicktemplate"
 )
 
-//line view/page/bookmark_home.qtpl:1
+//line view/page/bookmark_home.qtpl:3
 var (
 	_ = qtio422016.Copy
 	_ = qt422016.AcquireByteBuffer
 )
 
-//line view/page/bookmark_home.qtpl:1
+//line view/page/bookmark_home.qtpl:3
 func StreamBookmarkHome(qw422016 *qt422016.Writer) {
-//line view/page/bookmark_home.qtpl:1
+//line view/page/bookmark_home.qtpl:3
 	qw422016.N().S(`
-  <h1>Bookmarks</h1>
+  `)
+//line view/page/bookmark_home.qtpl:4
+	partial.StreamSubHeading(qw422016, "Bookmarks")
+//line view/page/bookmark_home.qtpl:4
+	qw422016.N().S(`
 `)
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 }
 
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 func WriteBookmarkHome(qq422016 qtio422016.Writer) {
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 	qw422016 := qt422016.AcquireWriter(qq422016)
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 	StreamBookmarkHome(qw422016)
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 	qt422016.ReleaseWriter(qw422016)
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 }
 
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 func BookmarkHome() string {
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 	qb422016 := qt422016.AcquireByteBuffer()
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 	WriteBookmarkHome(qb422016)
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 	qs422016 := string(qb422016.B)
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 	qt422016.ReleaseByteBuffer(qb422016)
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 	return qs422016
-//line view/page/bookmark_home.qtpl:3
+//line view/page/bookmark_home.qtpl:5
 }
