80ba06e872903425dd61baf8d69b993b46bb394c
Ajith Hussain <ajith.hussain@fastmail.com>
2021-03-07 20:51:51 +1100

    Fixed bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap/dist/js/bootstrap"
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("../stylesheets/main.scss")
