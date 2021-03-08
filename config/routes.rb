362ed2b6dc6f109c7258bcb78ae1ff7fc770da9d
Ajith Hussain <ajith.hussain@fastmail.com>
2021-03-08 23:01:29 +1100

    Updating

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
 
   resources :bookmarks, only: [:create]
+  resources :categories, only: [:index]
 
   get 'search', to: 'bookmarks#search', as: 'search'
   root to: 'bookmarks#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_134242) do
+ActiveRecord::Schema.define(version: 2021_03_08_114208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_02_27_134242) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.string "icon", default: "question"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_02_27_134242) do
   end
 
   add_foreign_key "bookmarks", "users"
+  add_foreign_key "categories", "users"
 end
