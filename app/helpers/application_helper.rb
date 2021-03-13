f4068a64bda38765a2270b61f9e0c7c048c767f9
Ajith Hussain <ajith.hussain@fastmail.com>
2021-03-13 19:09:27 +1100

    Styling add modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def icons
-    %i(question archive bag-fill briefcase-fill camera-reels-fill)
+    %i(question archive bag-fill briefcase-fill camera-reels-fill award-fill binoculars-fill bell-fill bookmark-fill box-seam calculator-fill bug-fill calendar-date-fill camera-fill card-image cart4 chat-left-dots-fill cloud collection-play-fill cpu-fill diagram-3-fill dice-5 droplet-fill display emoji-expressionless emoji-frown emoji-smile exclamation-circle-fill file-arrow-down-fill earbuds film flag-fill folder-fill lock-fill globe graph-up headphones image-fill rss-fill shop smartwatch tools facebook  twitter instagram youtube whatsapp palette person-fill play-circle-fill tools trash-fill truck vector-pen)
   end
 end
